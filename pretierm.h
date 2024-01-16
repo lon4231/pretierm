4d93257a34e656e81021cd995b17c266dd9a8df6
lon4231 <142271303+lon4231@users.noreply.github.com>
2024-01-16 18:40:25 -0300

    Update pretierm.h

--- pretierm.h
+++ pretierm.h
@@ -27,9 +27,14 @@
  #include<windows.h>
 #endif
 
+namespace pretierm
+{
+    
+
+
+#ifdef _WIN32
 void INIT_PRETIERM()
 {
-#ifdef _WIN32
  SetConsoleOutputCP(CP_UTF8);
  SetConsoleCP(CP_UTF8);
  HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -38,8 +43,9 @@ void INIT_PRETIERM()
  if (!GetConsoleMode(hConsole, &dwMode)){return;}
  dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
  if (!SetConsoleMode(hConsole, dwMode)){return;}
-#endif
+
 }
+#endif
 
 void clear_screen()
 {
@@ -63,7 +69,7 @@ std::cout<<"\033["<<y+1<<";"<<x+1<<"H";
 
 
 
-void draw_box(int x0,int y0,int x1,int y1,int sel_style)
+void draw_box(int x0,int y0,int x1,int y1,int selected_style)
 {
 if (x0>x1)
 {
@@ -89,26 +95,26 @@ std::vector<std::vector<std::string>>styles=
 };
 
 set_cursor_pos(x0,y0);
-std::cout<<styles[sel_style][0];
+std::cout<<styles[selected_style][0];
 for (size_t i = x0; i < x1-1; ++i)
 {
- std::cout<<styles[sel_style][5];
+ std::cout<<styles[selected_style][5];
 }
-std::cout<<styles[sel_style][1];
+std::cout<<styles[selected_style][1];
 for (size_t i = y0+1; i < y1; ++i)
 {
  set_cursor_pos(x0,i); 
- std::cout<<styles[sel_style][4];
+ std::cout<<styles[selected_style][4];
  set_cursor_pos(x1,i); 
- std::cout<<styles[sel_style][4];
+ std::cout<<styles[selected_style][4];
 }
 set_cursor_pos(x0,y1);
-std::cout<<styles[sel_style][2];
+std::cout<<styles[selected_style][2];
 for (size_t i = x0; i < x1-1; ++i)
 {
- std::cout<<styles[sel_style][5];
+ std::cout<<styles[selected_style][5];
 }
-std::cout<<styles[sel_style][3];
+std::cout<<styles[selected_style][3];
 }
 
 void draw_line(int x0,int y0,int x1,int y1,std::string fill)
@@ -121,8 +127,7 @@ void draw_line(int x0,int y0,int x1,int y1,std::string fill)
     int sy = (y0 < y1) ? 1 : -1;
     int err = dx - dy;
     while (true) {
-        set_cursor_pos(x0,y0);
-        std::cout<<fill;
+
         if (x0 == x1 && y0 == y1) {
             break;
         }
@@ -134,11 +139,14 @@ void draw_line(int x0,int y0,int x1,int y1,std::string fill)
         if (e2 < dx) {
             err = err + dx;
             y0 = y0 + sy;
+
         }
+        set_cursor_pos(x0,y0);
+        std::cout<<fill;
     }
 }
 
-void draw_progress_bar(int x,int y,int size,float pers,int sel_style)
+void draw_progress_bar(int x,int y,int bar_size,float perscentage,int selected_style)
 {
 std::vector<std::vector<std::string>>
 styles=
@@ -149,29 +157,41 @@ styles=
 {"[","/"," ","]"},
 };
 set_cursor_pos(x,y);
-float half=((size*0.01)*pers);
-std::cout<<styles[sel_style][0];
-for (size_t i = x; i < size; ++i)
+float half=((bar_size*0.01)*perscentage);
+std::cout<<styles[selected_style][0];
+for (unsigned short i = x; i < bar_size; ++i)
 {
  if (i<half)
- {std::cout<<styles[sel_style][1];}
+ {std::cout<<styles[selected_style][1];}
  else
- {std::cout<<styles[sel_style][2];}
+ {std::cout<<styles[selected_style][2];}
 }
-std::cout<<styles[sel_style][3]<<" "<<pers<<"%";
+std::cout<<styles[selected_style][3]<<" "<<perscentage<<"%";
 }
 
 
-void draw_line_graff(int x,int y,int size_bars,std::vector<float>persentages,std::string fill)
+void draw_line_graff(int x,int y,int bar_size,std::vector<float>persentages,std::string fill)
 {
- for (size_t i = 0; i < persentages.size(); i++)
+ for (unsigned short i = 0; i < persentages.size(); i++)
  {
-  for (size_t n = size_bars; n > (size_bars-(size_bars*0.01)*persentages[i]); --n)
+  for (unsigned short n = bar_size; n > (bar_size-(bar_size*0.01)*persentages[i]); --n)
   {
     set_cursor_pos(x+i,y+n);
     std::cout<<fill;
   }
   
+
+ }
+}
+
+void draw_triangle(int x0,int y0,int x1,int y1,int x2,int y2,std::string fill)
+{
+draw_line(x0,y0,x1,y1,fill);
+draw_line(x1,y1,x2,y2,fill);
+draw_line(x2,y2,x0,y0,fill);
+}
+
+}
 
  }
 }
