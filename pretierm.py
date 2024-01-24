999575542a15d2e34e61233ae008921951e727ac
lon4231 <142271303+lon4231@users.noreply.github.com>
2024-01-24 13:10:26 -0300

    Update pretierm.py

--- pretierm.py
+++ pretierm.py
@@ -11,26 +11,47 @@
 # ░░░░░                                                                            
 #provided by leon zabalegui
 
-
-import ctypes,os
-from ctypes import wintypes
+import os,random,time
 
 
 def INIT_PRETIERM():
  if os.name=="nt":
+    import ctypes
+    from ctypes import wintypes
     kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
     kernel32.SetConsoleOutputCP(ctypes.c_uint(65001))  # CP_UTF8
     kernel32.SetConsoleCP(ctypes.c_uint(65001))        # CP_UTF8
     STD_OUTPUT_HANDLE = -11
     hConsole = kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
     if hConsole == wintypes.HANDLE(-1):
-        
         return
     dwMode = ctypes.c_uint()
     if not kernel32.GetConsoleMode(hConsole, ctypes.byref(dwMode)):
         return
     dwMode.value |= 0x0004
     kernel32.SetConsoleMode(hConsole, dwMode)
+
+def set_console_font(font_size_x, font_size_y,font_name):
+ if os.name=="nt":
+  import ctypes
+  from ctypes import wintypes
+  class CONSOLE_FONT_INFOEX(ctypes.Structure):
+        _fields_ = [("cbSize", ctypes.c_ulong),
+                    ("nFont", ctypes.c_ulong),
+                    ("dwFontSize", wintypes._COORD),
+                    ("FontFamily", ctypes.c_uint),
+                    ("FontWeight", ctypes.c_uint),
+                    ("FaceName", wintypes.WCHAR * 32)]
+  font_info = CONSOLE_FONT_INFOEX()
+  font_info.cbSize = ctypes.sizeof(CONSOLE_FONT_INFOEX)
+  font_info.nFont = 0
+  font_info.dwFontSize.X = font_size_x
+  font_info.dwFontSize.Y = font_size_y
+  font_info.FontFamily = 0
+  font_info.FontWeight = 400
+  ctypes.windll.kernel32.lstrcpyW(font_info.FaceName, font_name)
+  handle = ctypes.windll.kernel32.GetStdHandle(-11)
+  ctypes.windll.kernel32.SetCurrentConsoleFontEx(handle, False, ctypes.byref(font_info))
 
 def clear_screen():
  print("\033[2J\033[H",end="")
@@ -39,10 +60,13 @@ def set_text_color(r=int,g=int,b=int):
  print(f"\033[38;2;{r};{g};{b}m",end="")
 
 def set_back_color(r=int,g=int,b=int):
- print(f"\033[38;2;{r};{g};{b}m",end="")
+ print(f"\033[48;2;{r};{g};{b}m",end="")
 
 def set_cursor_pos(x,y):
  print(f"\033[{y+1};{x+1}H",end="")
+
+def reset_text():
+ print("\033[0m",end="")
 
 def draw_box(x0,y0,x1,y1,selected_style):
  if x0>x1:
@@ -147,8 +171,13 @@ def draw_char_map(x,y,map):
   set_cursor_pos(x,y+i)
   print(map[i],end="")
 
-
-
+def draw_color_map(x,y,map):
+ for i in range(0,int(len(map))-1,2):
+  for n in range(len(map[i])):
+   set_cursor_pos(x+n,(y+int(i/2)))
+   set_back_color(map[i][n][0],map[i][n][1],map[i][n][2])
+   set_text_color(map[i+1][n][0],map[i+1][n][1],map[i+1][n][2])
+   print("▄",end="")
 
 
 
